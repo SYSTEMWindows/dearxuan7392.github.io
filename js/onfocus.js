f24c4c89c562d5e6f91172c21fdceb30155f99a0
DearXuan7392 <1320699768@qq.com>
2021-08-21 11:11:26 +0800

    Site updated: 2021-08-21 11:11:21

---
+++ js/onfocus.js
@@ -0,0 +1,17 @@
+// 浏览器搞笑标题
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "/favicon.ico");
+        document.title = '╭(°A°`)╮ 你去哪了? 快回来!!!';
+        clearTimeout(titleTime);
+    }
+    else {
+        $('[rel="icon"]').attr('href', "/favicon.ico");
+        document.title = '(ฅ>ω<*ฅ) 你终于回来了 ~';
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
